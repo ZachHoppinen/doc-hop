a78bf4059fe777f94dc1cf68092b3f843402ca70
Zachary Hoppinen <56941602+ZachHoppinen@users.noreply.github.com>
2024-01-26 14:20:34 -0500

    Update vectorize.py

--- code/geographic/vectorize.py
+++ code/geographic/vectorize.py
@@ -1,1 +1,10 @@
-
+from rasterio import features
+def vectorize_valid(fp):
+    """
+    Vectorize geotiff to valid pixels. Will only keep largest subset of pixels.
+    """
+    img = rxa.open_rasterio(fp).squeeze('band')
+    img = ~img.isnull()
+    img = img.astype('uint8')
+    mask = img == 1
+    return Polygon(list((features.shapes(img, mask = mask)))[0][0]['coordinates'][0])
