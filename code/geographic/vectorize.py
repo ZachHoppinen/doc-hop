222479da8e4371cc26d212de5b0c305c48df7d87
Zachary Hoppinen <56941602+ZachHoppinen@users.noreply.github.com>
2024-01-26 14:53:58 -0500

    Update vectorize.py

--- code/geographic/vectorize.py
+++ code/geographic/vectorize.py
@@ -4,4 +4,6 @@ def vectorize_valid(fp):
     img = ~img.isnull()
     img = img.astype('uint8')
     mask = img == 1
-    return Polygon(list((features.shapes(img, mask = mask)))[0][0]['coordinates'][0])
+    coords = list((features.shapes(img, mask = mask)))[0][0]['coordinates'][0]
+    xy_coords = [(img.x[int(x)].values.ravel()[0], img.y[int(y)].values.ravel()[0]) for x, y in coords]
+    return Polygon(xy_coords)
